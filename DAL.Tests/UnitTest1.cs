f3a7009906da5e24877e311269ab898b9f0ba665
iraandrushko <ira.andrushko2@gmail.com>
2022-10-16 23:53:12 +0300

    create TestsProject

---
+++ DAL.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DAL.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
